7a66bdf16732b7786f9db7ac1fef683ff950c642
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-27 15:29:44 -0300

    aprendnedo mais sobre eventos

--- 2_CONCEITOS/fundamentos/src/components/Events.jsx
+++ 2_CONCEITOS/fundamentos/src/components/Events.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
 export const Events = () => {
+
+    const handleClick = () => {
+        console.log("Executou");
+    }
+
+    // 8 - Função de renderização
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>
+        } else {
+            return <h1>Renderizando outra coisa!</h1>
+        }
+    }
     return (
         <div>
             <div>
                 <button onClick={() => console.log("Testando um evento")}>Clique aqui</button>
             </div>
+            <div>
+                {/* 7 - Evento com Função*/}
+                <button onClick={handleClick}>Clique Aqui - com função</button>
+            </div>
+            {/* 8 - Função de renderização*/}
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     ) 
 }
