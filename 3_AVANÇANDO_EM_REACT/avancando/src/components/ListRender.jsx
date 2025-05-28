af761666fb01a54d9d7ffc00990d82ae9944d5bd
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-28 12:55:04 -0300

    aprendendo sobre render com key

--- 3_AVANÇANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANÇANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -4,11 +4,25 @@ import { useState } from 'react'
 const ListRender = () => {
 
     const [list] = useState(["Luan", "Luna", "Lady"]);
+
+    const [users, setUsers] = useState([
+        {id: 1, name: "Luan", age: 20},
+        {id: 2, name: "Lady", age: 20},
+        {id: 3, name: "Luna", age: 0},
+    ])
+
     return (
         <div>
+            {/* 4 - render sem key*/}
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            {/* 5 - render com key*/}
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
         </div>
