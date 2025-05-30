1530226188e9b5a3674980d6bfc0dddcee559cb7
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-30 15:47:01 -0300

    Renderização de listas com componentes

--- 3_AVANÇANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANÇANDO_EM_REACT/avancando/src/App.jsx
@@ -18,6 +18,13 @@ import ShowUserName from './components/ShowUserName'
 // 9 - Desestruturando props
 import CarDetails from './components/CarDetails'
 
+// 11 - Renderização de listas com componente
+const cars = [
+  { id: 1, brand: "Ferrari", color: "Amarelo", km: 0},
+  { id: 1, brand: "KIA", color: "Branco", km: 20000},
+  { id: 1, brand: "Renault", color: "Azul", km: 32000}
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -39,6 +46,17 @@ function App() {
       <ShowUserName name="Luna"/>
       {/* 9 - Desestruturando props*/}
       <CarDetails brand="Ford" km={100000} color="Cinza"/>  
+      {/* 10 - Reaproveitando componentes*/}
+      <CarDetails brand="Fiat" km={999} color="Branco"/>
+      <CarDetails brand="Audi" km={500} color="Azul"/>
+      {/* 11 - Renderização de lista com componente*/}
+      {cars.map((car) => (
+        <CarDetails
+        key={car.id} 
+        brand={car.brand} 
+        color={car.color} 
+        km={car.km}/>
+      ))}
     </div>
   )
 }
