5f33504378b665495f84e5b6752f85515e0d70a7
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-31 08:44:23 -0300

    vendo o css em um componente especifico

--- 4_CSS_REACT/css_react/src/App.jsx
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// 2 - css de componente
+import MyComponent from './components/MyComponent'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +13,9 @@ function App() {
     <>
       {/* 1 - Css global*/}
       <h1>CSS NO REACT</h1>
+      {/* 2 - css de componente*/}
+      <MyComponent/>
+      <p>pegou o css do componente</p>
     </>
   )
 }
---
+++ 4_CSS_REACT/css_react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css";
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>CSS de componente</h1>
+            <p>este paragrafo vai ter um estilo</p>
+        </div>
+    )
+}
+
+export default MyComponent
