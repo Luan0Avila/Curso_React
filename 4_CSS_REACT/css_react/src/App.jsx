8448c68e8f82d238e70a3b89f7d40a13afdd0937
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-31 09:02:40 -0300

    finalizando o modulo

--- 4_CSS_REACT/css_react/src/App.jsx
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -6,8 +6,18 @@ import './App.css'
 // 2 - css de componente
 import MyComponent from './components/MyComponent'
 
+// 6 - CSS module
+import Title from './components/Title'
+
 function App() {
   const [count, setCount] = useState(0)
+
+  // 4 - css style dinamico
+  const n = 15;
+
+  // 5 - classes dinamicas
+  const redTitle = true;
+
 
   return (
     <>
@@ -18,6 +28,16 @@ function App() {
       <p>pegou o css do componente</p>
       {/* 3 - inline style*/}
       <p style={{color: "blue", padding: "25px", borderTop: "1px solid blue"}}>Este elemento tem estilos inline</p>
+      {/* 4 - inline style dinamico*/}
+      <h2 style={n > 10 ? {color: "purple"} : {color: "magenta"}}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 20 ? {color: "purple"} : {color: "magenta"}}>
+      CSS dinâmico 2
+      </h2>
+      {/* 5 - classes dinamicas*/}
+      <h2 className={redTitle ? "red-title" : "title"}>Esse titulo vai ter uma classe</h2>
+      <Title/>
     </>
   )
 }
