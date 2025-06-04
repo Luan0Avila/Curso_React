2d263defef3b926723eeabeca77c52749e98f679
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-04 11:09:36 -0300

    resgatando dados de json

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 1 - Resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    
+    async function getData() {
+      
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      console.log(data)
+
+      setProducts(data)
+    }
+    getData()
+  },[]);
 
   return (
     <>
       <h1>HTTP em React</h1>
+
+      {/* 1 - resgate de dados */}
+      {products.map((product) => (
+        <li key={product.id}>{product.name} - R${product.price}</li>
+      ))}
     </>
   )
 }
