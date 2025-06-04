93680efdf400f86112892972fa66c68b0651b4d1
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-04 11:25:16 -0300

    carregamento dinamico

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -42,8 +42,13 @@ function App() {
       },
       body: JSON.stringify(product),
     })
-  }
 
+    
+    // 3 - carregamento dinamico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+  };
 
 
   return (
