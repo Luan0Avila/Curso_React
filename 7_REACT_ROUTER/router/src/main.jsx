601edeff794732960fa1357cf89d18673e4989bb
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-10 19:22:56 -0300

    aprendendo sobre o Outlet

--- 7_REACT_ROUTER/router/src/main.jsx
+++ 7_REACT_ROUTER/router/src/main.jsx
@@ -5,17 +5,33 @@ import App from './App.jsx'
 
 import Contact from './routes/Contact.jsx'
 
+// 2 - pagina de erro
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// 1 - configurando router
 import { createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+
+// 3 - Componente base
+import Home from './routes/Home.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage/>,
+    // 3 - Componente base
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+      path: "contact",
+      element: <Contact/>,
+      }
+    ]
   },
-  {
-    path: "/contact",
-    element: <Contact/>
-  }
+
 ])
 
 createRoot(document.getElementById('root')).render(
