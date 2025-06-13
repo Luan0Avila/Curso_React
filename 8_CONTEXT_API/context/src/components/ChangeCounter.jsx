297677261ee3fc99addd5deb17cde3289558ab9f
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-13 19:36:16 -0300

    refatorando o contexto utilizando hooks

--- 8_CONTEXT_API/context/src/components/ChangeCounter.jsx
+++ 8_CONTEXT_API/context/src/components/ChangeCounter.jsx
@@ -1,9 +1,10 @@
-import { useContext} from "react"
 import { CounterContext } from "../context/CounterContext"
 
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const ChangeCounter = () => {
-    const {counter, setCounter} = useContext(CounterContext);
+    const {counter, setCounter} = useCounterContext(CounterContext);
     return (
         <div>
             <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
--- 8_CONTEXT_API/context/src/routes/Contact.jsx
+++ 8_CONTEXT_API/context/src/routes/Contact.jsx
@@ -1,13 +1,17 @@
 // 3 - alterando valor do context
-import { useContext } from "react"
+import ChangeCounter from "../components/ChangeCounter";
 import {CounterContext} from "../context/CounterContext";
 
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Contact = () => {
-  const {counter} = useContext(CounterContext);
+  const {counter} = useCounterContext(CounterContext);
   return (
     <div>
         <h1>Página de contato</h1>
         <p>Valor do contador: {counter}</p>
+        <ChangeCounter/>
     </div>
   )
 }
