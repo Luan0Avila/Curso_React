044a1aabdcf131244d22d687cc59a1d6b71c0923
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-19 17:14:09 -0300

    funções com ts no react

--- 10_react_com_ts/src/App.tsx
+++ 10_react_com_ts/src/App.tsx
@@ -6,12 +6,18 @@ function App() {
   const age: number = 21;
   const isWorking: boolean = true;
 
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá ${name}!`;
+  }
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
