293489cfde6732667f92f3be7f704b8a3533af64
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-19 19:58:39 -0300

    consertando o erro(estava errado na importação)

--- 10_react_com_ts/src/App.tsx
+++ 10_react_com_ts/src/App.tsx
@@ -4,9 +4,26 @@ import FIrstComponent from "./components/FIrstComponent";
 // 5 - desestruturando props
 import SecondComponent from "./components/SecondComponent";
 import Destructuring, {Category} from "./components/Destructuring";
-
+import { createContext } from "react";
 // 6 - useState
 import State from "./components/State";
+
+
+// 8 - type
+type textOrnull = string | null;
+
+type fixed = "Isso" | "Ou" | "Aquilo";
+
+// 9 - context
+interface IAppContext {
+  language: string,
+  framework: string,
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
+
+
 
 function App() {
 
@@ -20,19 +37,38 @@ function App() {
     return `Olá ${name}!`;
   }
 
+  // 8 - type
+  const myText:textOrnull = "Tem algum texto aqui"
+  let mySecondText:textOrnull = null;
+
+  const contextValue: IAppContext = {
+  language: "JavaScript",
+  framework: "React",
+  projects: 5
+};
+
+
+  //mySecondText = "Opa";
+  const testandoFixed:fixed = "Isso";
+
   return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && <p>Está trabalhando!</p>}
-      <h3>{userGreeting(name)}</h3>
-      <FIrstComponent/>
-      <SecondComponent name="Segundo"/>
-      <Destructuring title="Primeiro post" content="Algum conteúdo" commentQty={11} tags={["TypeScript", "JavaScript", "React"]} category={Category.TS}/>
-      <Destructuring title="Segundo post" content="Outro conteúdo" commentQty={110} tags={["Python", "Django"] } category={Category.P}/>
-      <State/>
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        <FIrstComponent/>
+        <SecondComponent name="Segundo"/>
+        <Destructuring title="Primeiro post" content="Algum conteúdo" commentQty={11} tags={["TypeScript", "JavaScript", "React"]} category={Category.TS}/>
+        <Destructuring title="Segundo post" content="Outro conteúdo" commentQty={110} tags={["Python", "Django"] } category={Category.P}/>
+        <State/>
+        {myText && 
+        <p>Tem texto na variável</p>}
+        {mySecondText && <p>Tem texto na variável</p>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
